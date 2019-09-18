96557f46577e84b349032f9416fcd216a118d00a
ianforrest11 <ianforrest@Ians-MBP.fios-router.home>
2019-09-18 18:56:22 -0400

    push lecture code to git

---
+++ module3-adding-data-science-to-a-web-application/assignment3/twitoff/app.py
@@ -0,0 +1,56 @@
+"""main application and routing logic for twitoff."""
+
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
+from .twitter import add_or_update_user
+
+def create_app():
+    """create/configure flask application."""
+    app = Flask(__name__)
+    
+    # config link to database and env from .env file
+    # remove flask shell warning about tracking modifications
+    # initiate app
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    DB.init_app(app)
+    
+    # create route to pass Users to application
+    # populate user object in html template
+    # sets title of application
+    @app.route('/')
+    def root():
+        users = User.query.all()
+        return render_template('base.html',
+                               title = 'Home',
+                               users = users)
+        
+    # define what to do when user enters /reset at end of URL
+    # resets app! clears users
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html',
+                               title = 'DB Reset!',
+                               users = [])
+    
+    # add user and user/<name>
+    @app.route('/user', methods=['POST']) # trigger this if post request (adding to db)
+    @app.route('/user/<name>', methods=['GET']) # trigger this if get request (pulling existing from db)
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets # 'one()' means pull first user
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
+    
+    return app
+    
